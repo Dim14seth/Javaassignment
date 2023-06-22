7712bf1f6afa9ee3a762623cc80b4e581070d2c6
Dim14seth <48237423+Dim14seth@users.noreply.github.com>
2023-06-22 13:08:29 +0530

    Add files via upload

---
+++ naturalno.java
@@ -0,0 +1,13 @@
+
+public class naturalno {
+	
+		public static void main(String[] args) {
+			
+			System.out.println("The First 10 Natural Numbers are");
+			for(int i = 1; i <= 10; i++) 
+			{
+				System.out.println(i);
+			}
+		}
+	}
+
