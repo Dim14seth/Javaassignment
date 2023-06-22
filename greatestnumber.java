892e8bcfe3d576f759fb11de720f4da47be5e037
Dim14seth <48237423+Dim14seth@users.noreply.github.com>
2023-06-22 12:32:10 +0530

    Add files via upload

---
+++ greatestnumber.java
@@ -0,0 +1,16 @@
+public class greatestnumber {
+
+    public static void main(String[] args) {
+
+        int n1 =25 , n2 = 78, n3 = 87;
+
+        if( n1 >= n2 && n1 >= n3)
+            System.out.println(n1 + " is the largest number.");
+
+        else if (n2 >= n1 && n2 >= n3)
+            System.out.println(n2 + " is the largest number.");
+
+        else
+            System.out.println(n3 + " is the largest number.");
+    }
+}
