f58e8e3d4ae146e734d920eef8080dd7022120d9
Dim14seth <48237423+Dim14seth@users.noreply.github.com>
2023-06-22 12:12:39 +0530

    Add files via upload

---
+++ positivenegative.java
@@ -0,0 +1,22 @@
+
+public class positivenegative {
+
+	public static void main(String[] args) {
+		int n=35;
+		if(n>0)
+		
+			{
+			System.out.println("Positive Number");
+			}
+			else if(n<0)
+			{
+				System.out.println("Negative Number");	
+			}
+			else
+			{
+				System.out.println("Number is Zero");
+			}
+		}
+	
+}
+
